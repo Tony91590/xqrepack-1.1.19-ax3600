c03c6d4cf210b851f46c201b6b57fef5edfd3a89
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2021-12-13 12:04:29 +0100

    Update repack-squashfs-mi.sh

--- repack-squashfs-mi.sh
+++ repack-squashfs-mi.sh
@@ -79,7 +79,6 @@ enable_dev_access() {
 	nvram set uart_en=1
 	nvram set ssh_en=1
 	nvram set boot_wait=on
-
 	nvram commit
 }
 
@@ -100,27 +99,27 @@ chmod 0755      "$FSDIR/sbin/xqflash"
 chown root:root "$FSDIR/sbin/xqflash"
 
 # dont start crap services
-for SVC in stat_points statisticsservice \
-		datacenter \
-		smartcontroller \
-		xq_info_sync_mqtt \
-		xiaoqiang_sync \
-		plugincenter plugin_start_script.sh cp_preinstall_plugins.sh; do
-	rm -f $FSDIR/etc/rc.d/[SK]*$SVC
-done
+#for SVC in stat_points statisticsservice \
+#		datacenter \
+#		smartcontroller \
+#		xq_info_sync_mqtt \
+#		xiaoqiang_sync \
+#		plugincenter plugin_start_script.sh cp_preinstall_plugins.sh; do
+#	rm -f $FSDIR/etc/rc.d/[SK]*$SVC
+#done
 
 # prevent stats phone home & auto-update
-for f in StatPoints mtd_crash_log logupload.lua otapredownload; do > $FSDIR/usr/sbin/$f; done
+#for f in StatPoints mtd_crash_log logupload.lua otapredownload; do > $FSDIR/usr/sbin/$f; done
 
-sed -i '/start_service(/a return 0' $FSDIR/etc/init.d/messagingagent.sh
+#sed -i '/start_service(/a return 0' $FSDIR/etc/init.d/messagingagent.sh
 
 # cron jobs are mostly non-OpenWRT stuff
-for f in $FSDIR/etc/crontabs/*; do
-	sed -i 's/^/#/' $f
-done
+#for f in $FSDIR/etc/crontabs/*; do
+#	sed -i 's/^/#/' $f
+#done
 
 # as a last-ditch effort, change the *.miwifi.com hostnames to localhost
-sed -i 's@\w\+.miwifi.com@localhost@g' $FSDIR/etc/config/miwifi
+#sed -i 's@\w\+.miwifi.com@localhost@g' $FSDIR/etc/config/miwifi
 
 # mark web footer so that users can confirm the right version has been flashed
 sed -i 's/romVersion%>/& xqrepack/;' "$FSDIR/usr/lib/lua/luci/view/web/inc/footer.htm"
